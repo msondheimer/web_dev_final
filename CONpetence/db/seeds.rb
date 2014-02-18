5da790399c3f5d17aab61e1d73329a8e93acb60c
Daniel Shiff <shiff.dan.e@gmail.com>
2014-02-18 15:26:53 -0600

    Worried

--- CONpetence/db/seeds.rb
+++ CONpetence/db/seeds.rb
@@ -7,62 +7,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+require 'date'
 
 all_convention_data = [ { :name => "Otakon",
 				 :genre => "Anime",
 				 :city => "Baltimore, MD",
 				 :venue => "Baltimore Convention Center",
 				 :con_url => "http://otakon.com",
-				 :expected_size => 35000,
-				 :start => "2013-08-09 09:00:00 EDT",
-				 :end => "2013-08-11 16:00:00 EDT"
-                },
-                {
-                	name: "Galifrey One",
-                	genre: "Franchise Specific",
-                	city: "Los Angeles, CA",
-                	venue: "LAX Marriott",
-                	con_url: "http://www.gallifreyone.com",
-                	expected_size: 3200,
-                	start: "2013-02-15 09:00:00 PST",
-                	:end => "2013-02-17 19:00:00 PST"
-                },
-                {
-                	name: "Gallfrey One",
-                	genre: "Franchise Specific",
-                	city: "Los Angeles, CA",
-                	venue: "LAX Marriott",
-                	con_url: "http://www.gallifreyone.com",
-                	expected_size: 3200,
-                	start: "2014-02-14 09:00:00 PST",
-                	:end => "2014-02-16 19:00:00 PST"
-                },
-                {
-                	name: "Anime Boston",
-                	genre: "Anime",
-                	city: "Boston, MA",
-                	venue: "Hynes Convention Center",
-                	con_url: "http://animeboston.com",
-                	expected_size: nil,
-                	start: "2014-03-21 09:00:00 EDT",
-                	:end => "2014-03-23 16:00:00 EDT"
-                }
+				 :expected_size => [29337, 30785, 34211, 35000],
+				 :start => ['2011-07-29','2012-07-27',"2013-08-09", '2014-08-08'],
+         :end => 3
+
+         }
+         {
+          name: "Gallifrey One"
+          city: "Los Angeles, CA"
+         }
             ]
 
 Convention.destroy_all
 
 all_convention_data.each do |convention_info|
-  c = Convention.new
-  c.name = convention_info[:name]
-  c.genre = convention_info[:genre]
-  c.city = convention_info[:city]
-  c.venue = convention_info[:venue]
-  c.con_url = convention_info[:con_url]
-  c.expected_size = convention_info[:expected_size]
-  c.start = convention_info[:start]
-  c.end = convention_info[:end]
-  c.save
+  convention_info[:expected_size].zip(convention_info[:start]).each do |con_size, con_start|
+    c = Convention.new
+    c.name = convention_info[:name]
+    c.genre = convention_info[:genre]
+    c.city = convention_info[:city]
+    c.venue = convention_info[:venue]
+    c.con_url = convention_info[:con_url]
+    c.expected_size = con_size
+    c.start = con_start
+    c.end = Date.parse(con_start.to_s) + convention_info[:end] - 1
+    c.save
+  end
 end
 
 otakon2013_photos = 
@@ -76,14 +53,14 @@ otakon2013_photos =
 	]
 
 
-Photo.destroy_all
-c = Convention.find_by(name: "Otakon")
-otakon2013_photos.each do |url|
-	p = Photo.new
-	p.photo_url = url
-	p.con = c.id
-	p.save
-end
+# Photo.destroy_all
+# c = Convention.find_by(name: "Otakon", :start.year => 2013)
+# otakon2013_photos.each do |url|
+# 	p = Photo.new
+# 	p.photo_url = url
+# 	p.con = c.id
+# 	p.save
+# end
 
 
 
