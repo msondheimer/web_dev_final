03a0b3038f8cc65678bbfb511dc87c45d5a92649
Daniel Shiff <shiff.dan.e@gmail.com>
2014-02-18 14:06:57 -0600

    don't like

--- CONpetence/app/models/convention.rb
+++ CONpetence/app/models/convention.rb
@@ -1,6 +1,30 @@
+require 'open-uri'
+require 'json'
+
 class Convention < ActiveRecord::Base
 	#Go to google to find coords
 	#after_save
+
+	after_save :find_coords
+
+	def find_coords
+		if self.city == nil
+			self.lat = nil
+			self.lon = nil
+		else
+			address = self.city.chomp
+			if self.venue != nil
+				address = (self.venue.chomp + address)
+			end
+			address = address.tr(" ", "+")
+			json_data = open("http://maps.googleapis.com/maps/api/geocode/json?address=#{address}&sensor=true").read()
+			data = JSON.parse(json_data)
+			self.lat = data['results'][0]['geometry']['location']['lat']
+			self.lon = data['results'][0]['geometry']['location']['lng']
+		end
+		return self.lat, self.lon
+	end
+
 
 	scope :future, -> {where("end > ?", Time.now).order("start asc")}
 
@@ -9,7 +33,7 @@ class Convention < ActiveRecord::Base
 	def Convention.present
 		return Convention.where("start < ? AND end > ?", Time.now, Time.now)
 	end
-	scope :present -> 
+	#scope :present -> 
 
 	scope :genre, ->(gen) {where("genre = ?", gen)}
 end
