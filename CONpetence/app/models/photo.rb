f05a28628d38546f61604b27242fd85f6ec38fff
Daniel Shiff <shiff.dan.e@gmail.com>
2014-02-20 19:01:34 -0600

    Something not working in gem

--- CONpetence/app/models/photo.rb
+++ CONpetence/app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
 
-	has_attached_file :picture
-	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+	#has_attached_file :picture
+	#validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
 	belongs_to :convention, foreign_key: "con"
 
