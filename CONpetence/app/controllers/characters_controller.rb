06d656b9c579ff6c06977f6bcef91b3100b01f84
Daniel Shiff <shiff.dan.e@gmail.com>
2014-03-13 17:55:51 -0500

    Characters now only editable by users.

--- CONpetence/app/controllers/characters_controller.rb
+++ CONpetence/app/controllers/characters_controller.rb
@@ -1,4 +1,12 @@
 class CharactersController < ApplicationController
+	before_action :require_login, :except => [:index, :show]
+
+	def require_login
+      if session[:user_id].blank?
+        redirect_to "/characters", notice: "Sign in to do that!"
+      end
+    end
+
 	def new
 		render 'new'
 	end
